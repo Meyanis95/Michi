67e0867feac26489cea5466d691b167019ad21e5
Thomas Cherret <thomas.cherret@papernest.com>
2022-02-02 19:34:41 +0100

    add db

--- front/src/routes/form.js
+++ front/src/routes/form.js
@@ -1,40 +1,39 @@
 import '../form.css';
 import axios from 'axios';
 import React, { Component, useState } from 'react';
-import env from "react-dotenv";
-var CryptoJS = require("crypto-js");
+import env from 'react-dotenv';
+var CryptoJS = require('crypto-js');
 const FormData = require('form-data');
+import { supabase } from '../helpers/db.js';
 
 function Form() {
-
   const [selectedVideoFile, setSelectedVideoFile] = useState(null);
   const [selectedImageFile, setSelectedImageFile] = useState(null);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [content, setContent] = useState('');
 
-  const onVideoChange = event => {
+  const onVideoChange = (event) => {
     setSelectedVideoFile(event.target.files[0]);
   };
 
-  const onTitleChange = event => {
-    setTitle(event.target.value)
-  }
+  const onTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
 
-  const onImageChange = event => {
+  const onImageChange = (event) => {
     setSelectedImageFile(event.target.files[0]);
   };
 
-  const onDescriptionChange = event => {
-    setDescription(event.target.value)
-  }
+  const onDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
 
-  const onContentChange = event => {
-    setContent(event.target.value)
-  }
+  const onContentChange = (event) => {
+    setContent(event.target.value);
+  };
 
   const onFileUpload = () => {
-
     const pinVideoFileToIPFS = async (pinataApiKey, pinataSecretApiKey) => {
       const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
       let data = new FormData();
@@ -53,14 +52,14 @@ function Form() {
           regions: [
             {
               id: 'FRA1',
-              desiredReplicationCount: 1
+              desiredReplicationCount: 1,
             },
             {
               id: 'NYC1',
-              desiredReplicationCount: 2
-            }
-          ]
-        }
+              desiredReplicationCount: 2,
+            },
+          ],
+        },
       });
       data.append('pinataOptions', pinataOptions);
 
@@ -70,18 +69,18 @@ function Form() {
           headers: {
             'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
             pinata_api_key: pinataApiKey,
-            pinata_secret_api_key: pinataSecretApiKey
-          }
+            pinata_secret_api_key: pinataSecretApiKey,
+          },
         })
         .then(function (response) {
-          console.log(response)
-          return 'https://ipfs.io/ipfs/' + response.data.IpfsHash
+          console.log(response);
+          return 'https://ipfs.io/ipfs/' + response.data.IpfsHash;
         })
         .catch(function (error) {
-          console.log(error)
+          console.log(error);
         });
 
-      return result
+      return result;
     };
 
     const pinImageFileToIPFS = async (pinataApiKey, pinataSecretApiKey) => {
@@ -102,14 +101,14 @@ function Form() {
           regions: [
             {
               id: 'FRA1',
-              desiredReplicationCount: 1
+              desiredReplicationCount: 1,
             },
             {
               id: 'NYC1',
-              desiredReplicationCount: 2
-            }
-          ]
-        }
+              desiredReplicationCount: 2,
+            },
+          ],
+        },
       });
       data.append('pinataOptions', pinataOptions);
 
@@ -119,18 +118,18 @@ function Form() {
           headers: {
             'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
             pinata_api_key: pinataApiKey,
-            pinata_secret_api_key: pinataSecretApiKey
-          }
+            pinata_secret_api_key: pinataSecretApiKey,
+          },
         })
         .then(function (response) {
-          console.log(response)
-          return 'https://ipfs.io/ipfs/' + response.data.IpfsHash
+          console.log(response);
+          return 'https://ipfs.io/ipfs/' + response.data.IpfsHash;
         })
         .catch(function (error) {
-          console.log(error)
+          console.log(error);
         });
 
-      return result
+      return result;
     };
 
     const pinJSONToIPFS = (pinataApiKey, pinataSecretApiKey, JSONBody) => {
@@ -139,11 +138,11 @@ function Form() {
         .post(url, JSONBody, {
           headers: {
             pinata_api_key: pinataApiKey,
-            pinata_secret_api_key: pinataSecretApiKey
-          }
+            pinata_secret_api_key: pinataSecretApiKey,
+          },
         })
         .then(function (response) {
-          console.log(response)
+          console.log(response);
         })
         .catch(function (error) {
           //handle error here
@@ -151,44 +150,47 @@ function Form() {
     };
 
     async function main() {
-      const ipfsVideoUrl = await pinVideoFileToIPFS(env.PINATA_KEY, env.PINATA_SECRET_KEY)
-      const ipfsImageUrl = await pinImageFileToIPFS(env.PINATA_KEY, env.PINATA_SECRET_KEY)
+      const ipfsVideoUrl = await pinVideoFileToIPFS(
+        env.PINATA_KEY,
+        env.PINATA_SECRET_KEY
+      );
+      const ipfsImageUrl = await pinImageFileToIPFS(
+        env.PINATA_KEY,
+        env.PINATA_SECRET_KEY
+      );
       const dataJson = {
-        'ipfs_video_url': ipfsVideoUrl,
-        'ipfs_backgroung_url': ipfsImageUrl,
-        'metadata': {
-          'title': title,
-          'description': description,
-          'content': content
-        }
-      }
-      var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(dataJson), 'secret key 123').toString();
-      console.log(ciphertext)
-      pinJSONToIPFS(env.PINATA_KEY, env.PINATA_SECRET_KEY, ciphertext)
+        ipfs_video_url: ipfsVideoUrl,
+        ipfs_backgroung_url: ipfsImageUrl,
+        metadata: {
+          title: title,
+          description: description,
+          content: content,
+        },
+      };
+      var ciphertext = CryptoJS.AES.encrypt(
+        JSON.stringify(dataJson),
+        'secret key 123'
+      ).toString();
+      console.log(ciphertext);
+      pinJSONToIPFS(env.PINATA_KEY, env.PINATA_SECRET_KEY, ciphertext);
     }
 
     main();
   };
 
   const fileData = () => {
-
     if (selectedVideoFile) {
-
       return (
         <div>
           <h2>File Details:</h2>
 
           <p>File Name: {selectedVideoFile.name}</p>
 
-
           <p>File Type: {selectedVideoFile.type}</p>
 
-
           <p>
-            Last Modified:{" "}
-            {selectedVideoFile.lastModifiedDate.toDateString()}
+            Last Modified: {selectedVideoFile.lastModifiedDate.toDateString()}
           </p>
-
         </div>
       );
     } else {
@@ -199,31 +201,47 @@ function Form() {
         </div>
       );
     }
+
+    // TO ADD hash to the db once we have the address of the contract + the generated hash, to run in the console while waiting
+    // const { data, error } = await supabase.from('lesson_hash').insert([
+    //   {
+    //     course_address: 'gingigg',
+    //     hash: 'ifongoingiogn',
+    //   },
+    // ]);
+    // if (error) {
+    //   console.log(error);
+    // } else {
+    //   console.log(data);
+    // }
   };
 
   return (
     <div>
-      <h1>
-        Web3 Udemy
-      </h1>
-      <h3>
-        Create your first course!
-      </h3>
+      <h1>Web3 Udemy</h1>
+      <h3>Create your first course!</h3>
       <div>
         <label>Title</label>
         <input type="text" value={title} onChange={onTitleChange}></input>
         <label>Upload your video content</label>
         <input type="file" onChange={onVideoChange} />
         <label>Description</label>
-        <input type="text" value={description} onChange={onDescriptionChange}></input>
+        <input
+          type="text"
+          value={description}
+          onChange={onDescriptionChange}
+        ></input>
         <label>Upload a background image</label>
         <input type="file" onChange={onImageChange} />
         <label>Content</label>
-        <input id="textarea" type="textarea" value={content} onChange={onContentChange}></input>
+        <input
+          id="textarea"
+          type="textarea"
+          value={content}
+          onChange={onContentChange}
+        ></input>
         <br />
-        <button onClick={onFileUpload}>
-          Upload!
-        </button>
+        <button onClick={onFileUpload}>Upload!</button>
       </div>
       {fileData()}
     </div>
