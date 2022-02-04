afd5c579500477877596d7edaf9da7aac8371cf8
Meyanis95 <yanis.meziane@essec.edu>
2022-02-04 10:50:52 +0100

    update

--- front/src/views/mydashboard.jsx
+++ front/src/views/mydashboard.jsx
@@ -5,6 +5,7 @@ import abi from "../utils/CourseFactory.json";
 
 export default function MyDashBoard() {
   const [allCoursesCreated, setAllCoursesCreated] = useState([]);
+  const [arrPrice, setArrPrice] = useState([]);
   const contractAddress = "0x88E94879A723541EDfb814258224CBB4819981D0"
   const contractABI = abi.abi
 
@@ -18,10 +19,38 @@ export default function MyDashBoard() {
 
       const getAllCourses = async () => {
         const allContracts = courseFactoryContract.getCourses("0xFe48Eb58b0B889E57844F50B4da7B1886F680C4F")
-        allContracts.then(v => console.log(v))
+        const all_addresses = []
+        allContracts.then(v =>
+          all_addresses.push(v)
+        )
+        return all_addresses
       };
 
-      getAllCourses();
+      const fetchPrice = (_arrayAddress) => {
+        const arr = []
+        _arrayAddress.forEach(course => {
+          const price = getPrice(course)
+          arr.push(price)
+          console.log("price", price)
+        })
+        console.log(arr)
+        setArrPrice(arr);
+        return arr;
+      }
+
+      const getPrice = (_address) => {
+        let courseContract = new ethers.Contract(_address, contractABI, signer);
+        const price = courseContract.getAmount()
+        return price
+      }
+
+      const main = async () => {
+        console.log("ca démarre")
+        const all = await getAllCourses();
+        console.log("toutes les adresses", all)
+        fetchPrice(all)
+      }
+      main()
 
     } else {
       console.log('Ethereum object not found');
@@ -31,6 +60,7 @@ export default function MyDashBoard() {
   return (
     <div>
       <h1> Hello World!</h1>
+      {arrPrice}
     </div>
   );
 }
